753ffef1ab5afe526c61ce602ba927e8a35dbf84
Justin Webb <webb.n.justin@gmail.com>
2020-07-18 22:13:09 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -55,8 +55,9 @@ def add_characters
    }
 
    epic_tragedy [:additional_characters] = [
-      { :name => "Prince Escalus" },
-      { :name => "Apothecary" }
+     prince = { :name => "Prince Escalus" }
+     
+     apothecary = { :name => "Apothecary" }
     ]
   
 
